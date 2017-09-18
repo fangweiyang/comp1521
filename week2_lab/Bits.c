17cb82834c74142461748060aebf137c3431dc01
FangWeiyang <307544377@qq.com>
2017-09-18 16:00:19 +1000

    Update Bits.c

--- week2_lab/Bits.c
+++ week2_lab/Bits.c
@@ -1,8 +1,3 @@
-// ADT for Bit-strings
-// COMP1521 17s2 Week02 Lab Exercise
-// Written by John Shepherd, July 2017
-// Modified by ...
-
 #include <stdlib.h>
 #include<math.h>
 #include <stdio.h>
@@ -33,7 +28,7 @@ int power(int exponent);
 long decimalToBinary(int n);
 char *BitList(int i);
 void leftShiftBits_case_1(Bits b, int shift, Bits res);
-void leftShiftBits_case_2(Bits b, int shift, Bits res)
+void leftShiftBits_case_2(Bits b, int shift, Bits res);
 // make a new empty Bits with space for at least nbits
 // rounds up to nearest multiple of BITS_PER_WORD
 Bits makeBits(int nbits)
@@ -66,7 +61,6 @@ void  freeBits(Bits b)
 void andBits(Bits a, Bits b, Bits res){
   int i,max=0;
   max = (a->nwords >= b->nwords)? a->nwords : b->nwords;
-  //less = (a->nwords >= b->nwords)? b->nwords : a->nwords;
   for(i = 0;i < max; i++){
      res ->words[i]= a->words[i] & b->words[i];
   }
@@ -78,7 +72,6 @@ void andBits(Bits a, Bits b, Bits res){
 void orBits(Bits a, Bits b, Bits res){
   int i,max=0;
   max = (a->nwords >= b->nwords)? a->nwords : b->nwords;
-  //less = (a->nwords >= b->nwords)? b->nwords : a->nwords;
   for(i = 0;i < max; i++){
      res->words[i] = a->words[i]|b->words[i];
   }
@@ -95,27 +88,70 @@ void invertBits(Bits a, Bits res){
    res->nwords=a->nwords;
 }
 
-// left shift Bits
-void leftShiftBits(Bits b, int shift, Bits res){
-  if(b->nwords==1){
-    if(shift>32) shift=32;
-    leftShiftBits_case_1(b,shift,res);
-    return;
-  }elseif(shift> b->nwords* 32){
-    shift=32;
-    leftShiftBits_case_1(b,shift,res);
-    return;
-  }else{
-    leftShiftBits_case_2(b,shift,res);
-    return;
-  }
-}
-
 // right shift Bits
 void rightShiftBits(Bits b, int shift, Bits res){
+   int i=0;
+   unsigned int overflow=0;
+   if(shift>=32* b->nwords){
+      for(i=0;i<b->nwords;i++){
+         res->words[i]=0;
+      }
+      return ;
+   }
+   Bits temp= makeBits(64);
+   setBitsFromBits(b,temp);
+   for(; shift >= 32;shift = shift - 31){
+      for (i = b->nwords-1;i >= 0; i--){
+         overflow=0;
+         temp->words[i]= b->words[i] >> 31;
+         if(i > 0 ){
+             overflow=b->words[i-1] << 1;
+             temp->words[i]= temp->words[i]+overflow;
+         }
+      }
+   }
+   int unshift=32-shift;
+   for(i=temp->nwords-1;i >=0;i--){
+      overflow=0;
+      res->words[i]=(temp->words[i] >> shift);
+      if(i>0) {
+         overflow=temp->words[i-1] << unshift;
+         res->words[i]= res->words[i]+overflow;
+      }
+   }
+   res->nwords=b->nwords;
+}
+
+// left shift Bits
+void leftShiftBits(Bits b, int shift, Bits res){
   int i=0;
-  for (i=0;i < b->nwords;i++){
-    res->words[i]=b->words[i] >> shift;
+  if(shift>=32* b->nwords){
+      for(i=0;i<b->nwords;i++){
+         res->words[i]=0;
+      }
+      return ;
+   }
+  Bits temp= makeBits(64);
+  setBitsFromBits(b,temp);
+  unsigned int overflow=0;
+  for(;shift>=32;shift=shift-31){
+      for (i=0;i<b->nwords;i++){
+         overflow=0;
+         temp->words[i]= b->words[i] << 31;
+         if(i > 0){
+             overflow=b->words[i] >> 1;
+             temp->words[i-1]= temp->words[i-1]+overflow;
+         }
+     }
+   }
+   int unshift=32-shift;
+   for (i=0;i < b->nwords;i++){
+       overflow=0;
+       res->words[i]= (temp->words[i] << shift);
+       if(i > 0){
+           overflow=temp->words[i] >> unshift;
+           res->words[i-1]= res->words[i-1]+overflow;
+       }
   }
   res->nwords=b->nwords;
 }
@@ -135,12 +171,26 @@ void setBitsFromBits(Bits from, Bits to){
 // assign a bit-string (sequence of 0's and 1's) to Bits
 // if the bit-string is longer than the size of Bits, truncate higher-order bits
 void setBitsFromString(Bits b, char *bitseq){
- int i,j=0;
-   unsigned int result=0;
-   for(j=0;j < b->nwords;j++){
+   int i,j=0;
+    unsigned int result=0;
+   char *ptr = bitseq;
+   int len=strlen(bitseq);
+   if (len < b->nwords * 32) {
+      for (i = b->nwords * 32 - 1; i >= b->nwords * 32 - len; i--){
+         ptr[i] = ptr[i - b->nwords * 32 + len];
+      }
+      for (i = 0; i < b->nwords * 32 - len; i++) {
+         ptr[i] = '0';
+      }
+         ptr[b->nwords * 32] = '\0';
+   } else {
+         int diff = len - b->nwords * 32;
+         for (i = 0; i < b->nwords * 32; i++) ptr[i] = ptr[diff + i];
+   }
+   for(j=0;j< b->nwords;j++){
       result=0;
-      for(i=32-strlen(bitseq);i<32;i++,bitseq++){
-         if(*bitseq=='1')
+      for(i=31;i>=0;i--,ptr++){
+         if(*ptr=='1')
              result=result+power(i);
       }
       b->words[j]=result;
@@ -150,7 +200,7 @@ void setBitsFromString(Bits b, char *bitseq){
 // display a Bits value as sequence of 0's and 1's
 void showBits(Bits b){
    int i;
-   for(i=b->nwords-1;i >=0;i--){
+   for(i=0;i < b->nwords;i++){
       printBit(b->words[i]);
    }
 }
@@ -187,7 +237,7 @@ void printBit(int i){
       }
       i=i>>1;
    }
-   for(j = 0;j<32;j++){
+   for(j = 31;j>=0;j--){
        printf("%d",list[j]);
    }
 }
@@ -209,23 +259,4 @@ char *BitList(int i){
 
 
 
-void leftShiftBits_case_1(Bits b, int shift, Bits res){
-   int i=0;
-   for (i=0;i < b->nwords;i++){
-     res->words[i]=b->words[i] << shift;
-   }
-   res->nwords=b->nwords;
-}
 
-void leftShiftBits_case_2(Bits b, int shift, Bits res){
-  int i=0;
-  unsigned int overflow=0;
-  for (i=0;i < b->nwords;i++){
-    res->words[i]=b->words[i] << shift;
-    if(i>1){
-        overflow==b->words[i-1] >> 32-shift;
-        res->words[i]=res->words[i]+overflow;
-    }
-  }
-  res->nwords=b->nwords;
-  }
